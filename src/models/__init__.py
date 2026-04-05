2e76caef89578f8122407b74de719ab86d405e82
Rayan <66010947+Rayan1088@users.noreply.github.com>
2026-04-05 13:54:24 +0100

    Update efficientnet_b7 import to efficientnet_b6

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -8,7 +8,7 @@ from .resnet import (
     resnet50,
     resnet50_fc512,
 )
-from .tvmodels import mobilenet_v3_small, vgg16, efficientnet_b7
+from .tvmodels import mobilenet_v3_small, vgg16, efficientnet_b6
 
 
 __model_factory = {
